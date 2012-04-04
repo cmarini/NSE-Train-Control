95621e46d87e56e4d96a1f16f2168f5b1e390c8e
Christian Marini <j.christian.marini@gmail.com>
2012-04-04 12:55:07 -0400

    Updated formatting as per the Code Convention

--- Wayside.java
+++ Wayside.java
@@ -1,13 +1,16 @@
-public class Wayside implements Runnable {
+public class Wayside implements Runnable
+{
 	private ArrayList<Track> track;
 	private PLC plc;
 	private String id;
+	
 	public Wayside nextRight;
 	public Wayside nextLeft;
 	public Wayside prevRight;
 	public Wayside prevLeft;
 	
-	public Wayside(String id, Wayside nextRight, Wayside nextLeft, Wayside prevRight, Wayside prevLeft) {
+	public Wayside(String id, Wayside nextRight, Wayside nextLeft, Wayside prevRight, Wayside prevLeft)
+	{
 		this.id = id;
 		this.nextRight = nextRight;
 		this.nextLeft = nextLeft;
@@ -16,39 +19,47 @@ public class Wayside implements Runnable {
 		this.track = new ArrayList<Track>();
 	}
 	
-	public void run() {
+	public void run()
+	{
 		
 	}
 	
-	public void setAuthority(String trainID, int authority) {
+	public void setAuthority(String trainID, int authority)
+	{
 		/* VALIDATE */
 		/* Find specified train */
 		/* set train's authority */
 	}
 	
-	public void setSpeedLimit(String trackID, int speed) {
+	public void setSpeedLimit(String trackID, int speed)
+	{
 		/* VALIDATE */
 		/* Find specified track */
 		/* set track's speed limit */
 	}
 	
-	private Track findTrack(String trackID) {
+	private Track findTrack(String trackID)
+	{
 		/* Scan track section for ID */
 	}
 	
-	private Train findTrain(String trainID) {
+	private Train findTrain(String trainID)
+	{
 		/* Scan track section for specified train ID */
 	}
 	
-	public void addTrack(Track t) {
+	public void addTrack(Track t)
+	{
 		track.add(t);
 	}
 	
-	public int getID() {
+	public String getID()
+	{
 		return id;
 	}
 
-	public String toString() {
+	public String toString()
+	{
 		return new String();
 	}
 }
