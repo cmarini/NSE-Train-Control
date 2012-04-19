ee443b57871fe0f1e265229d99f03e9f4f029ee5
Christian Marini <j.christian.marini@gmail.com>
2012-04-19 11:36:20 -0400

    Added SwitchState enum

--- trackmodel/Switch.java
+++ trackmodel/Switch.java
@@ -2,7 +2,9 @@ package trackmodel;
 
 public class Switch extends Track
 {
-	private boolean switchState = false; //defult setting
+	public enum SwitchState { LEFT, RIGHT; }
+	
+	private SwitchState switchState = SwitchState.LEFT; //defult setting
 	private String sInfo;
 	
 	public Switch(double iElevate, double iGrade, int spLimit, String trkID, int X1, int Y1, int X2, int Y2)
@@ -10,13 +12,13 @@ public class Switch extends Track
 		super (iElevate, iGrade, spLimit, trkID, X1, Y1, X2, Y2);
 	}
 
-	public void setSwitchState(boolean state) //set switch state
+	public void setSwitchState(SwitchState state) //set switch state
 	{
 		switchState = state;
-		sInfo = "info: switch state set to: "+switchState;		
+		sInfo = "info: switch state set to: " + switchState;		
 	}
 	
-	public boolean getSwitchState()	//get switch state
+	public SwitchState getSwitchState()	//get switch state
 	{
 		sInfo = "info: sent switch state";
 		return switchState;
