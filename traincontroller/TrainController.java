cbc1e8f7befbf669a08882f86d83c4bb4de6de31
Shatara Washington <washington.shatara@yahoo.com>
2012-04-25 14:58:22 -0400

    Update TrainController

--- traincontroller/TrainController.java
+++ traincontroller/TrainController.java
@@ -8,13 +8,15 @@ import java.util.logging.*;
 public class TrainController implements Runnable
 { 
     private Train train;
-	private Track track; 
+	 private Track track; 
     private GreenSchedule G;
     private RedSchedule R;
     private String id;
     private boolean stopped;
     private boolean onSchedule;
-    private boolean braking;
+    private boolean emergencyBraking;
+    private boolean serviceBrake; 
+    private boolean emergencyBrake;       //velocity - used in calcPower 
     private double setPoint;
     private double currentSpeed;          //Speed returned by Train Model 
     private double operatorSpeed;         //Speed entered by user 
@@ -26,18 +28,17 @@ public class TrainController implements Runnable
     private double prev_d;                //previous d value
     private double prev_v;                //previous v value 
     private double d;                     //setPoint - currentSpeed
-    private double v;                     //velocity - used in calcPower 
+    private double v; 
+    private double authority;    
     private static int clockRate;
     private Line scheduleLine;
     private int crew;
     private int ran = 1; 
     private String message;
-	private int timewaited; 
-	private int clockticValue; 
+	 private int timewaited; 
+	 private int clockticValue;
 	
-
-	//add a clockrate 1ms * 60/Rate rates - time given  function to let CTC set the clock rate
-	// constructor - line int, crew count int, id string, clock rate intger  Train - Train train = new Train and whatever his constructor is 
+	
     public TrainController(Line line, int crewCount, int clock, String idVal, Track T) 
     {
         scheduleLine = line;
@@ -48,10 +49,9 @@ public class TrainController implements Runnable
         train = new Train(scheduleLine, crew, id);
         message = "";
     }
-
+		
     public void run() 
     {
-        System.out.println("Inside TrainControllers Run Method.");
         updateTrain();
         updateSchedule();
     }
@@ -81,40 +81,40 @@ public class TrainController implements Runnable
         return braking;
     }
 
-    public double calcPower(double currentSpeed, double operatorSpeed, double waysideSpeed) 
+    public double calcPower(double currentSpeed, double operatorSpeed) 
     {
+    		
+    	//if authority is less than 1 - stop in that block 
         double trainPeriod = 0; //Sample period of train model = 60/clockrate/1000 
         double Kp = 2;          //proportional gain (Guess value)
         double Ki = 1;          //integral (Guess value)
-			
-			//What happens if train is braking?? 
-        if (ran == 1) 
+		
+		  prev_v = train.getVelocity(); 
+		  waysideSpeed = getSpeedLimit(); 
+		  authority = getAuthority(); 
+		  
+		  
+		  if (ran == 1) 
         {
             power = init_power;
             ran++;
         }
-		
-		//when your going slower than you want to go or someone pulls the emergency break 
-		if(setpoint lower than speed currently going
-		{
-			brake
-		}
-		
-		if(braking = true) 
-		{
-			//set braking to the decelerate speed...
-			
-		}        
-		
-		if (scheduleLine.equals(Line.GREEN)) 
+        
+        if(authority < 1)
+		  {
+		  		train.setServiceBrake(serviceBrake); 
+				power = 0; 
+		  }
+        
+        if (scheduleLine.equals(Line.GREEN)) 
         {
-            onSchedule = G.onSchedule();
+        		onSchedule = G.onSchedule();
         } 
         else if (scheduleLine.equals(Line.RED)) 
         {
-            onSchedule = R.onSchedule();
+        		onSchedule = R.onSchedule();
         }
-
+        
         if (onSchedule == false) //means off schedule 
         {
             setPoint = waysideSpeed;
@@ -123,21 +123,36 @@ public class TrainController implements Runnable
         {
             setPoint = operatorSpeed;
         }
-
-        if (power < maxPower) 
-        {
-            trainPeriod = (60 / clockRate) * .001; 
+	
+		if(currentSpeed > setPoint) //when your going slower than you want to go or someone pulls the emergency break
+		{
+			train.setServiceBrake(serviceBrake); 
+			power = 0; 
+		}
+		else if(currentSpeed < setPoint)
+		{
+			if (power < maxPower) 
+         {
+        		trainPeriod = (60 / clockRate) * .001; 
             d = setPoint - currentSpeed;
             v = prev_v + (trainPeriod / 2) * (d + prev_d);
-        } 
-        else if (power >= maxPower) 
-        {
+         } 
+         else if (power >= maxPower) 
+         {
             v = prev_v; //v= v(n-1)
-        }
+         }
 
         power = Kp * d + Ki * v;
-
-        return power;
+			
+		}
+		 
+		if(emergencyBraking = true) 
+		{
+			train.setEmergencyBrake(emergencyBrake);
+			power = 0; 
+		}        
+		
+		return power;
     }
 
     /*
@@ -149,8 +164,7 @@ public class TrainController implements Runnable
         String info;
         power = calcPower(currentSpeed, operatorSpeed, waysideSpeed);  
 		train.setPower(power); 
-		//Service braking -- 1.2m/s^2 
-		//emergency brake -- 2.73 m/s^2
+		
 
         if (train.hasTransponder()) 
         {
@@ -177,8 +191,8 @@ public class TrainController implements Runnable
 
     public void updateSchedule() 
     {
-        System.out.println("Inside TrainControllers updateSchedule Method.");
-        //scheduleLine = train.getLine();
+        scheduleLine = train.getLine();
+        
         if (scheduleLine.equals(Line.GREEN)) 
         {
             G.greenSchedule();
@@ -191,8 +205,7 @@ public class TrainController implements Runnable
 
     public boolean trainStopped() 
     {
-        System.out.println("Inside TrainControllers Stopped Method.");
-        //velocity = train.getVelocity();
+        velocity = train.getVelocity();
         if (velocity == 0) {
             stopped = true;
         } 
@@ -205,21 +218,16 @@ public class TrainController implements Runnable
 
     public double getClockRate() 
     {
-        System.out.println("Inside TrainControllers getClockRate Method.");
         return clockRate;
     }
 
     public void setclockRate(int clock) 
     {
-        System.out.println("Inside TrainControllers setClockRate Method.");
+       
         clock = clockRate;
     }
 	
-	public void setWaysideSpeed(double w) 
-	{
-		waysideSpeed = w;  
-	}
-	
+		
 	public void setOperatorSpeed(double opSpeed) 
 	{
 		operatorSpeed = opSpeed; 
